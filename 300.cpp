9ad50a29a8ef2f5aaafaa5135c20514902e6c701
ktakada42 <ktakada950711@gmail.com>
2023-02-04 23:22:20 +0900

    feat: 300. Longest Increasing Subsequence

---
+++ 300.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+		vector<int> dp(nums.size(), 0);
+		dp[0] = 1;
+		int ans = 1;
+		for (int i = 1; i < nums.size(); i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (nums[j] < nums[i] && dp[i] < dp[j]) {
+					dp[i] = dp[j];
+				}
+			}
+			dp[i]++;
+			ans = max(ans, dp[i]);
+		}
+		return (ans);
+	}
+};
